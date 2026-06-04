0841fbb958d46edf8cd9385136a4468e0b3656ae
Adin Farel <fareladin00@gmail.com>
2026-06-04 21:48:45 +0700

    feat: add normalization for normalize hidden states before compute attn and ffwd

---
+++ basemodel/src/normalization.py
@@ -0,0 +1,22 @@
+'''
+basemodel/src/normalization.py
+
+Build normalization layer for normalize hidden states to prevent vanishing/exploding gradient, dying ReLU, saturated Tanh
+'''
+
+from turtle import forward
+
+import torch
+import torch.nn as nn
+
+class RMSNorm(nn.Module):
+    def __init__(self, embed_dim: int, eps=1e-8):
+        super().__init__()
+        self.embed_dim = embed_dim
+        self.weight = nn.Parameter(torch.ones(embed_dim))
+        self.eps = eps
+    
+    def forward(self, x: torch.Tensor):
+        rms = torch.rsqrt(x.pow(2).mean(dim=-1, keepdim=True) + self.eps)
+        x_norm = x * rms
+        return x_norm * self.weight
